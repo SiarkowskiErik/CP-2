07c16ede385fe4d4441b0527aa408ef70aec743f
SiarkowskiErik <rm93535@fiap.com.br>
2022-10-01 20:03:48 -0300

    Estrutura basica

---
+++ pet/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PetCadastro from './components/PetCadastro'
+import './App.css'
+
+export default function App() {
+    return (
+        <div>
+            <h1>CP2 - 2° Semestre</h1>
+            <PetCadastro/>
+        </div>
+    )
+}
